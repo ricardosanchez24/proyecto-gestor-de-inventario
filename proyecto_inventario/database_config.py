78fecb7a6fe6e07efca6c6f72d38e64234b010e5
Ricardo Sanchez <ricardosanchez24.2006@gmail.com>
2026-01-11 03:30:05 -0400

    todavia con las tablas

--- proyecto_inventario/database_config.py
+++ proyecto_inventario/database_config.py
@@ -9,6 +9,6 @@ if not db_url:
     db_url = "mysql+pymysql://2H3RDvaTTfeiDqR.root:EjqCDvJx9loyrWWX@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=<CA_PATH>&ssl_verify_cert=true&ssl_verify_identity=true"
 
 #db_url = 'mysql+pymysql://root:123456789@localhost/gestor_inventario'
-engine = create_engine(db_url, pool_pre_ping=True) # hace la conexion con la base de datos
+engine = create_engine(db_url) # hace la conexion con la base de datos
 SessionLocal = sessionmaker(bind=engine) # crea una fabrica de sesiones no las sesiones en si
 Base = declarative_base() # crear un cajon de etiquetas para que sqlalchemy sepa que codigo tiene que traducir a sql
