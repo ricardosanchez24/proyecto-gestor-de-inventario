8bb9bef77b8a7794d75ceeb7a55b406c05ccae0f
Ricardo Sanchez <ricardosanchez24.2006@gmail.com>
2026-01-11 02:14:16 -0400

    actualizacion de la url de la db

--- proyecto_inventario/database_config.py
+++ proyecto_inventario/database_config.py
@@ -6,7 +6,7 @@ import os
 db_url = os.environ.get('DATABASE_URL')
 
 if not db_url:
-    db_url = "mysql+pymysql://2H3RDvaTTfeiDqR.root:9QOIPNSg1l2wDmxF@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=/etc/ssl/certs/ca-certificates.crt&ssl_verify_cert=true&ssl_verify_identity=true"
+    db_url = "mysql+pymysql://2H3RDvaTTfeiDqR.root:EjqCDvJx9loyrWWX@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=<CA_PATH>&ssl_verify_cert=true&ssl_verify_identity=true"
 
 #db_url = 'mysql+pymysql://root:123456789@localhost/gestor_inventario'
 engine = create_engine(db_url, pool_pre_ping=True) # hace la conexion con la base de datos
